0ad91cd80f2c379489acd0b1585dd5bc55d2b78a
mahendransri <mahendransri999@gmail.com>
2023-10-01 11:29:21 +0530

    dev done his

--- src/org/testing/Computer.java
+++ src/org/testing/Computer.java
@@ -1,7 +1,12 @@
 package org.testing;
 
 public class Computer {
-public void computerModel() {
-System.out.println("system 1");
-}
+
+	public void computerModel() {
+		System.out.println("system 1");
+	}
+	private void namedev() {
+		System.out.println("hello sri");
+
+	}
 }
