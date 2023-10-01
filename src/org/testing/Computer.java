5c6d710a62c105b2d5834b6e6a199b8686f98fd9
mahendransri <mahendransri999@gmail.com>
2023-10-01 18:40:25 +0530

    priya done her code

--- src/org/testing/Computer.java
+++ src/org/testing/Computer.java
@@ -1,7 +1,12 @@
 package org.testing;
 
 public class Computer {
-public void computerModel() {
-System.out.println("system 1");
-}
+
+	public void computerModel() {
+		System.out.println("system 1");
+	}
+
+	public static void main(String[] args) {
+		Computer c1 = new Computer();
+	}
 }
